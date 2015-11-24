f530f285506c0c17ce03cabcc98c3f0d66db33b2
Michael McMillan <email@michaelmcmillan.net>
2015-11-25 00:13:04 +0100

    tests and implementation for changing the volume and model names

--- src/speaker.js
+++ src/speaker.js
@@ -7,7 +7,46 @@ class SpeakerService {
 }
 
 class Speaker {
-    constructor() {
+
+    constructor () {
 
     }
+
+    get volume () {
+        return this._volume;
+    }
+
+    set volume (level) {
+        this._volume = this.refine_volume(level);
+    }
+
+    get model () {
+        return this._model;
+    }
+
+    set model (name) {
+        if (this.is_valid_model(name))
+            this._model = name;
+    }
+
+    is_valid_model (name) {
+        if (['Play 1', 'Play 3', 'Play 5'].indexOf(name) >= 0)
+            return true;
+        throw new Error('Illegal model name.');
+    }
+
+    refine_volume (level) {
+        if (level > 100)
+            return 100;
+        else if (level < 0)
+            return 0;
+        else
+            return level;
+    }
+
+    stream (url) {
+        throw new Error('Invalid URL.');
+    }
 }
+
+module.exports = Speaker;
--- test/speaker.test.js
+++ test/speaker.test.js
@@ -1,11 +1,49 @@
 var assert = require('assert');
+var Speaker = require('../src/speaker.js');
 
 describe('Speaker', function () {
 
     describe('volume', function () {
         
-        it('should', function () {});
+        it('can not go over 100', function () {
+            var speaker = new Speaker(); 
+            speaker.volume = 101
+            assert.equal(speaker.volume, 100);
+        });
 
+        it('can not go under 0', function () {
+            var speaker = new Speaker(); 
+            speaker.volume = 101
+            assert.equal(speaker.volume, 100);
+        });
+
+    });
+
+    describe('model', function () {
+
+        it('raises exception if name is "Play X"', function () {
+            var speaker = new Speaker();
+            assert.throws(function () {
+                speaker.model = 'Play X';
+            }, /model/);
+        });
+
+        it('does not raise exception if name is "Play 5"', function () {
+            var speaker = new Speaker();
+            speaker.model = 'Play 5';
+            assert.equal(speaker.model, 'Play 5');
+        });
+
+    });
+
+    describe('stream', function () {
+        
+        it('raises exception if given an invalid URL', function () {
+            var speaker = new Speaker();
+            assert.throws(function () {
+                speaker.stream('htp://soundcloud.com/invalid-url/song.mp3');
+            }, /URL/);
+        });
     });
 
 });
