e72a452816ee5cdab084c788a94d123380e499d6
Michael McMillan <email@michaelmcmillan.net>
2015-11-29 10:40:18 +0100

    roll back to old unit test

--- test/speaker.test.js
+++ test/speaker.test.js
@@ -1,16 +1,49 @@
 var assert = require('assert');
 var Speaker = require('../src/speaker.js');
-var app = require('app');
-var BrowserWindow = require('browser-window');
 
-describe('Integration', function () {
+describe('Speaker', function () {
 
-    describe('integration', function () {
+    describe('volume', function () {
         
         it('can not go over 100', function () {
-            assert.equal(1, 1);
+            var speaker = new Speaker(); 
+            speaker.volume = 101
+            assert.equal(speaker.volume, 100);
         });
 
+        it('can not go under 0', function () {
+            var speaker = new Speaker(); 
+            speaker.volume = -1 
+            assert.equal(speaker.volume, 0);
+        });
+
+    });
+
+    describe('model', function () {
+
+        it('raises exception if name is "Play X"', function () {
+            var speaker = new Speaker();
+            assert.throws(function () {
+                speaker.model = 'Play X';
+            }, /model/);
+        });
+
+        it('does not raise exception if name is "Play 5"', function () {
+            var speaker = new Speaker();
+            speaker.model = 'Play 5';
+            assert.equal(speaker.model, 'Play 5');
+        });
+
+    });
+
+    describe('stream', function () {
+        
+        it('raises exception if given an invalid URL', function () {
+            var speaker = new Speaker();
+            assert.throws(function () {
+                speaker.stream('htp://soundcloud.com/invalid-url/song.mp3');
+            }, /URL/);
+        });
     });
 
 });
